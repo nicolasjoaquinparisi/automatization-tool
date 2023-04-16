fbc8cf6c89f844baaa651e46b4f13ac4e280bb8f
Nicolás Joaquín Parisi <78542476+NicolasJoaquinParisi@users.noreply.github.com>
2023-04-16 18:06:07 -0300

    added example workflows

---
+++ workflows/httpRequest-example.js
@@ -0,0 +1,18 @@
+import { HTTPRequestNode } from "../src/nodes/HTTPRequestNode/HTTPRequestNode.js";
+
+async function main() {
+  const httpRequestNode = new HTTPRequestNode({
+    url: "https://pokeapi.co/api/v2/pokemon?offset=20&limit=151",
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  // API Request
+  const { results: pokemons } = await httpRequestNode.execute();
+
+  console.log(pokemons);
+}
+
+main();
