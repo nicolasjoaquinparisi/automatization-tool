ef415a814a98c2f32c111ce10c08a313dfd3096d
Nicolás Joaquín Parisi <78542476+NicolasJoaquinParisi@users.noreply.github.com>
2023-04-19 21:35:02 -0300

    added new postgres node validators

--- src/nodes/PostgresNode/utils/validators.js
+++ src/nodes/PostgresNode/utils/validators.js
@@ -1,9 +1,22 @@
 import Joi from "joi";
 
+const setupSchema = Joi.object({
+  user: Joi.string().required(),
+  password: Joi.alternatives().try(Joi.string(), Joi.number()).required(),
+  host: Joi.string().required(),
+  password: Joi.alternatives().try(Joi.string(), Joi.number()).required(),
+  database: Joi.string().required(),
+});
+
+const insertSchema = Joi.object({
+  table: Joi.string().required(),
+  data: Joi.object().required().keys().min(1),
+});
+
 const updateSchema = Joi.object({
   table: Joi.string().required(),
   set: Joi.object().required(),
   where: Joi.string().required(),
 });
 
-export { updateSchema };
+export { setupSchema, insertSchema, updateSchema };
